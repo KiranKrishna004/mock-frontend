7f2ded91334356245426fe3dab2d9c87b0d6d1d6
KiranKrishna004 <kiran.004narath@gmail.com>
2024-09-23 04:35:27 +0530

    feat: add react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,18 @@ import '@mantine/core/styles.css'
 import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import './index.css'
 import { HeaderMegaMenu } from './components/HeaderMegaMenu/index.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ColorSchemeScript defaultColorScheme="auto" />
     <MantineProvider defaultColorScheme="auto">
-      <HeaderMegaMenu />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <HeaderMegaMenu />
+        <App />
+      </QueryClientProvider>
     </MantineProvider>
   </StrictMode>
 )
