7cf025250feaf1eef5dc7e68557be214ed6d06b3
KiranKrishna004 <kiran.004narath@gmail.com>
2024-09-23 04:58:11 +0530

    feat: add login services

---
+++ src/service/AuthService.ts
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import { BASE_URL } from '../constants'
+
+interface LoginResponse {
+  token: string
+}
+
+interface LoginRequest {
+  username: string
+  password: string
+}
+
+interface SignupRequest {
+  email: string
+  username: string
+  password: string
+}
+
+interface SignupResponse {
+  id: number
+  email: string
+  username: string
+  password: string
+  name: {
+    firstname: string
+    lastname: string
+  }
+  address: {
+    city: string
+    street: string
+    number: number
+    zipcode: string
+    geolocation: {
+      lat: string
+      long: string
+    }
+  }
+  phone: string
+}
+
+export const Login = (userAuth: LoginRequest): Promise<LoginResponse> =>
+  axios
+    .post(`${BASE_URL}/auth/login`, JSON.stringify(userAuth))
+    .then((res: { data: LoginResponse }) => res.data)
+
+export const Signup = (userInfo: SignupRequest): Promise<SignupResponse> =>
+  axios.post(`${BASE_URL}/users`, JSON.stringify(userInfo))
